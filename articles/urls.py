98c3bb812b6306ccf39db7173c0d1811c2102321
pangpyo <kkp0639@gmail.com>
2022-12-05 17:46:08 +0900

    feat : 댓글, 대댓글 기능 추가 및 아티클과 한 번에 응답하게 한다

--- articles/urls.py
+++ articles/urls.py
@@ -2,9 +2,32 @@ from django.urls import path, include
 from rest_framework import routers
 from . import views
 
-router = routers.DefaultRouter()
-router.register("articles", views.ArticleViewSet, basename="article")
-router.register("comment", views.CommentViewSet, basename="comment")
 app_name = "articles"
+router = routers.DefaultRouter()
+router.register("", views.ArticleViewSet, basename="article")
+# router.register("comment", views.CommentViewSet, basename="comment")
 
-urlpatterns = [path("", include(router.urls))]
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path(
+        "<int:article_pk>/comment",
+        views.CommentViewSet.as_view({"post": "create", "get": "list"}),
+    ),
+    path(
+        "<int:article_pk>/comment/<int:pk>",
+        views.CommentViewSet.as_view(
+            {"put": "update", "patch": "partial_update", "delete": "destroy"}
+        ),
+    ),
+    path(
+        "<int:article_pk>/comment/<int:comment_pk>/recomment",
+        views.ReCommentViewSet.as_view({"post": "create", "get": "list"}),
+    ),
+    path(
+        "<int:article_pk>/comment/<int:comment_pk>/recomment/<int:pk>",
+        views.ReCommentViewSet.as_view(
+            {"put": "update", "patch": "partial_update", "delete": "destroy"}
+        ),
+    ),
+]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,6 @@
-from .serializers import ArticleSerializer, CommentSerializer
+from .serializers import ArticleSerializer, CommentSerializer, ReCommentSerializer
 from rest_framework import viewsets
-from .models import Article, Comment
+from .models import Article, Comment, ReComment
 
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .permissions import IsOwnerOrReadOnly
@@ -9,19 +9,40 @@ from .permissions import IsOwnerOrReadOnly
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
+    serializer_class = ArticleSerializer
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
-
+    serializer_class = CommentSerializer
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        serializer.save(
+            user=self.request.user,
+            article=Article.objects.get(pk=self.kwargs.get("article_pk")),
+        )
+
+    def get_queryset(self):
+        return super().get_queryset().filter(article=self.kwargs.get("article_pk"))
+
+
+class ReCommentViewSet(viewsets.ModelViewSet):
+    serializer_class = ReCommentSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = ReComment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(
+            user=self.request.user,
+            article=Article.objects.get(pk=self.kwargs.get("article_pk")),
+            parent=Comment.objects.get(pk=self.kwargs.get("comment_pk")),
+        )
+
+    def get_queryset(self):
+        return super().get_queryset().filter(parent=self.kwargs.get("comment_pk"))
