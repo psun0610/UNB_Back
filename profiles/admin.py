1f3cfaaaeff77f4373d55827b42d20f8927b8c82
pangpyo <kkp0639@gmail.com>
2022-12-07 11:30:07 +0900

    feat : 뱃지 모델링 및 시리얼라이저 추가한다

---
+++ profiles/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+admin.site.register(Badge)
+admin.site.register(UserBadge)
+admin.site.register(UsingBadge)
---
+++ profiles/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from .models import *
+
+
+class BadgeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Badge
+        fields = "__all__"
+
+
+class UserBadgeSerializer(serializers.HyperlinkedModelSerializer):
+    user = serializers.ReadOnlyField(source="user.nickname")
+    badge = BadgeSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = UserBadge
+        fields = [
+            "pk",
+            "user",
+            "badge",
+        ]
+
+
+class UsingBadgeSerializer(serializers.HyperlinkedModelSerializer):
+    user = serializers.ReadOnlyField(source="user.nickname")
+    badge = BadgeSerializer(read_only=True)
+
+    class Meta:
+        model = UsingBadge
+        fields = [
+            "pk",
+            "user",
+            "badge",
+        ]
