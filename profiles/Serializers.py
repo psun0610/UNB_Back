d0b3e27e38139d789164e2cc557a40c2ad6313d2
Pangpyo <108651596+Pangpyo@users.noreply.github.com>
2022-12-08 02:07:20 +0900

    Delete Serializers.py

--- profiles/Serializers.py
+++
@@ -1,8 +0,0 @@
-from profiles.models import Score
-from rest_framework import serializers
-
-
-class ScoreSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Score
-        fields = ("user", "total")
